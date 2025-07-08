60afb46879eb84b43a8732478cf1e9ed8cfe7fab
Samara Bastos <samarabastos.dev@gmail.com>
2025-07-08 19:15:22 -0300

    build: configurando o projeto

---
+++ src/test/java/com/notificacao/service/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notificacao.service.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
