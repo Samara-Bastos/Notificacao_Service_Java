a66dead78cd553a42a4e4d349a83596fa8355337
Samara Bastos <samarabastos.dev@gmail.com>
2025-07-10 20:53:07 -0300

    fix: limpando o codigo

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,24 +24,11 @@ repositories {
 }
 
 dependencies {
-	// Spring Boot
 	implementation("org.springframework.boot:spring-boot-starter")
-
-	// Kafka Consumer
 	implementation("org.springframework.kafka:spring-kafka")
-
-	// Jackson (JSON)
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-
-	// PostgreSQL + JPA
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	runtimeOnly("org.postgresql:postgresql")
-
-	// Testes
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.kafka:spring-kafka-test")
-
-	// Lombok
 	compileOnly("org.projectlombok:lombok:1.18.30")
 	annotationProcessor("org.projectlombok:lombok:1.18.30")
 }
